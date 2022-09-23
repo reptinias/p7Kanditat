b0dcdb3ae0ab6bd011956c2f9a6c3e7ecec6e052
UrbanizedGnu <43436310+UrbanizedGnu@users.noreply.github.com>
2022-09-23 13:18:30 +0200

    Updates to animation system
    +It is now possible to record multiple animations and have them saved in runtime. Use K to start recording and P to end recording.
    
    +Cleaned up movement and animation code
    
    +Comments

--- OculusQuest/Assets/Scripts/AnimationRecorder.cs
+++ OculusQuest/Assets/Scripts/AnimationRecorder.cs
@@ -1,43 +1,77 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
 
 public class AnimationRecorder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public AnimationClip clip;
-
     private GameObjectRecorder m_Recorder;
+    public GameObject target;
+    public AnimatorController controller;
+    private bool recording = false;
+    private AnimationClip clip;
     
-    void Start()
-    {
-        // Create recorder and record the script GameObject.
-        m_Recorder = new GameObjectRecorder(gameObject);
-
-        // Bind all the Transforms on the GameObject and all its children.
-        m_Recorder.BindComponentsOfType<Transform>(gameObject, true);
-    }
-
-    // Update is called once per frame
     void LateUpdate()
     {
-        if (Input.GetKey("p"))
-            this.enabled = false;
+        //If the program is recording and the p-key is pressed, stop recording
+        if (recording && Input.GetKey("p"))
+        {
+            StopRecording();
+            recording = false;
+        }
 
-        if (clip == null)
-            return;
-        m_Recorder.TakeSnapshot(Time.deltaTime);
+        //If the program is NOT recording and the k-key is pressed, start recording
+        if (recording == false && Input.GetKey("k"))
+        {
+            recording = true;
+            StartRecording(target, controller);
+        }
+        //Records changes to the object and saves them to the recorder
+        if (recording)
+        {
+            if (clip == null)
+                return;
+            m_Recorder.TakeSnapshot(Time.deltaTime);
+        }
+        
     }
-    void OnDisable()
+
+    //Called to start recording. Takes a target object and that target's AnimatorController as arguments(will fix later)
+    //Changes to be made: Find target object's AnimatorController automatically
+    void StartRecording(GameObject target, AnimatorController controller)
+    {
+        m_Recorder = new GameObjectRecorder(target);
+
+        // Bind all the Transforms on the GameObject and all its children.
+        m_Recorder.BindComponentsOfType<Transform>(target, true);
+        //Clears clip data
+        clip = null;
+        clip = new AnimationClip();
+        
+        //Safeguard. Dunno if it does anything anymore
+        target.GetComponent<Animator>().StopPlayback();
+    }
+
+    //Called to stop recording
+    void StopRecording()
     {
         if (clip == null)
             return;
 
         if (m_Recorder.isRecording)
         {
+            //Creates a new animation clip in the designated path
+            AssetDatabase.CreateAsset(clip, "Assets/Test" + System.DateTime.Now.Minute.ToString() + System.DateTime.Now.Second.ToString() + ".anim");
+            //Saves clip to AnimatorController
+            controller.AddMotion(clip);
             // Save the recorded session to the clip.
             m_Recorder.SaveToClip(clip);
+            
+            //Safeguard. Dunno if it does anything anymore
+            target.GetComponent<Animator>().StopPlayback();
+            
         }
     }
 }
--- OculusQuest/Assets/Scripts/TestMovement.cs
+++ OculusQuest/Assets/Scripts/TestMovement.cs
@@ -8,27 +8,28 @@ public class TestMovement : MonoBehaviour
     public GameObject self;
     
     
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
+    //Very basic movement script. Only used for debugging of animation
     void Update()
     {
         if (Input.GetKey("w"))
             Move(Vector3.forward);
-        else if (Input.GetKey("s"))
+        if (Input.GetKey("s"))
             Move(Vector3.back);
-        else if (Input.GetKey("d"))
+        if (Input.GetKey("d"))
             Move(Vector3.right);
-        else if (Input.GetKey("a"))
+        if (Input.GetKey("a"))
             Move(Vector3.left);
     }
 
     void Move(Vector3 direction)
     {
-        self.transform.position += direction * speed * Time.deltaTime;
+        self.transform.position += Time.deltaTime*direction * speed;
     }
 }
