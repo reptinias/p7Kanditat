259261c024b8febd06678a14208ef81a1f469099
reptinias <k.spar@hotmail.com>
2022-11-18 14:55:09 +0100

    :meat_on_bone: :see_no_evil:

--- OculusQuest/Assets/Scripts/AnimationPlayer.cs
+++ OculusQuest/Assets/Scripts/AnimationPlayer.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 public class AnimationPlayer : MonoBehaviour
 {
+    private Animator anim;
+    public GameObject target;
+    private AnimatorController controller;
+    private Selection selector;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         
+        controller = target.GetComponent<Animator>().runtimeAnimatorController as AnimatorController;
     }
 
     // Update is called once per frame
