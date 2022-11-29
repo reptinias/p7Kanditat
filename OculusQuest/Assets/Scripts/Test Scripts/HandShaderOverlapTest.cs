8219716d485a600f4f02cf003a6f73d9217b66eb
Herrelaekker <jacobhlnhtxb16@gmail.com>
2022-11-29 09:13:11 +0100

    hand material works!!! ??!?!??! !

---
+++ OculusQuest/Assets/Scripts/Test Scripts/HandShaderOverlapTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandShaderOverlapTest : MonoBehaviour
+{
+    private Color[] fingerColor = { Color.black, Color.blue, Color.yellow, Color.red, Color.green, Color.white };
+    SkinnedMeshRenderer smr;
+    private void Start()
+    {
+        smr = GetComponentInChildren<SkinnedMeshRenderer>();
+
+        for (int i = 0; i < smr.materials.Length; i++)
+        {
+            smr.materials[i].SetColor("_Color", fingerColor[i]);
+        }
+
+    }
+
+
+}
